df51de60906c455a501e5008ce14630e85880c0c
YUYU <yuyu22300032@gmail.com>
2025-01-04 17:39:20 +0800

    add 1930

---
+++ 1930.cpp
@@ -0,0 +1,84 @@
+/*
+1930. Unique Length-3 Palindromic Subsequences
+
+
+Description:
+
+Given a string s, return the number of unique palindromes of length three that are a subsequence of s.
+
+Note that even if there are multiple ways to obtain the same subsequence, it is still only counted once.
+
+A palindrome is a string that reads the same forwards and backwards.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+    For example, "ace" is a subsequence of "abcde".
+
+ 
+
+Example 1:
+
+Input: s = "aabca"
+Output: 3
+Explanation: The 3 palindromic subsequences of length 3 are:
+- "aba" (subsequence of "aabca")
+- "aaa" (subsequence of "aabca")
+- "aca" (subsequence of "aabca")
+
+Example 2:
+
+Input: s = "adc"
+Output: 0
+Explanation: There are no palindromic subsequences of length 3 in "adc".
+
+Example 3:
+
+Input: s = "bbcbaba"
+Output: 4
+Explanation: The 4 palindromic subsequences of length 3 are:
+- "bbb" (subsequence of "bbcbaba")
+- "bcb" (subsequence of "bbcbaba")
+- "bab" (subsequence of "bbcbaba")
+- "aba" (subsequence of "bbcbaba")
+
+ 
+
+Constraints:
+
+    3 <= s.length <= 105
+    s consists of only lowercase English letters.
+
+*/
+
+class Solution {
+public:
+    struct helper {
+        set<char> mid_char;
+        int prev_cnt;
+
+        helper()
+        : mid_char()
+        , prev_cnt(0) {};
+    };
+
+    int countPalindromicSubsequence(string s) {
+        int out = 0;
+        map<char, helper> search_map;
+        for (int i = 1; i < s.size(); ++i)
+        {
+            map<char, helper>::iterator it = search_map.find(s[i]);
+            if (it != search_map.end())
+            {
+                out += it->second.mid_char.size() - it->second.prev_cnt;
+                it->second.prev_cnt = it->second.mid_char.size();
+            
+            }
+            for (map<char, helper>::iterator mit = search_map.begin(); mit != search_map.end(); ++mit)
+            {
+                mit->second.mid_char.insert(s[i]);
+            }
+            search_map[s[i-1]].mid_char.insert(s[i]);
+        }
+        return out;
+    }
+};
