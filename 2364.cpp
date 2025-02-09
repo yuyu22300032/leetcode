3db6abcf1b976a5e5851d5d5ed76a4e84279793b
YUYU <yuyu22300032@gmail.com>
2025-02-09 09:03:13 +0800

    add 2364

---
+++ 2364.cpp
@@ -0,0 +1,73 @@
+/*
+2364. Count Number of Bad Pairs
+
+
+Description:
+
+You are given a 0-indexed integer array nums. A pair of indices (i, j) is a bad pair if i < j and j - i != nums[j] - nums[i].
+
+Return the total number of bad pairs in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,1,3,3]
+Output: 5
+Explanation: The pair (0, 1) is a bad pair since 1 - 0 != 1 - 4.
+The pair (0, 2) is a bad pair since 2 - 0 != 3 - 4, 2 != -1.
+The pair (0, 3) is a bad pair since 3 - 0 != 3 - 4, 3 != -1.
+The pair (1, 2) is a bad pair since 2 - 1 != 3 - 1, 1 != 2.
+The pair (2, 3) is a bad pair since 3 - 2 != 3 - 3, 1 != 0.
+There are a total of 5 bad pairs, so we return 5.
+
+Example 2:
+
+Input: nums = [1,2,3,4,5]
+Output: 0
+Explanation: There are no bad pairs.
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 105
+    1 <= nums[i] <= 109
+
+
+*/
+
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        unordered_map<int, int> dmap;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            int diff = nums[i] - i;
+            unordered_map<int, int>::iterator it = dmap.find(diff);
+            if (it == dmap.end())
+            {
+                dmap.insert(pair<int, int>(diff, 1));
+            }
+            else
+            {
+                it->second += 1;
+            }
+        }
+        long long good = 0;
+        for (unordered_map<int, int>::iterator it = dmap.begin(); it != dmap.end(); ++it)
+        {
+            if (it->second == 1)
+            {
+                continue;
+            }
+            long long cur = it->second;
+            cur = cur * (cur - 1) / 2;
+            good += cur;
+        }
+        long long out = nums.size();
+        out = out * (out - 1) / 2 - good;
+        return out;
+    }
+};
+
