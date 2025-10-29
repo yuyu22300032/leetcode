d10b11fd59b59f433a12216a73e904258b8193e8
YUYU <yuyu22300032@gmail.com>
2025-10-29 23:41:32 +0800

    add 3370

---
+++ 3370.cpp
@@ -0,0 +1,60 @@
+/*
+3370. Smallest Number With All Set Bits
+
+
+Description:
+
+You are given a positive number n.
+
+Return the smallest number x greater than or equal to n, such that the binary representation of x contains only
+
+ 
+
+Example 1:
+
+Input: n = 5
+
+Output: 7
+
+Explanation:
+
+The binary representation of 7 is "111".
+
+Example 2:
+
+Input: n = 10
+
+Output: 15
+
+Explanation:
+
+The binary representation of 15 is "1111".
+
+Example 3:
+
+Input: n = 3
+
+Output: 3
+
+Explanation:
+
+The binary representation of 3 is "11".
+
+ 
+
+Constraints:
+
+    1 <= n <= 1000
+
+*/
+
+class Solution {
+public:
+    int smallestNumber(int n) {
+        int out = 1;
+        while (out < n) {
+            out = out * 2 + 1;
+        }
+        return out;
+    }
+};
