32f9c63f6619a06290ab2d021b0d1506930a4057
YUYU <yuyu22300032@gmail.com>
2026-01-04 19:50:30 +0800

    add 334

---
+++ 0334.cpp
@@ -0,0 +1,58 @@
+/*
+334. Increasing Triplet Subsequence
+
+
+Description:
+
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+
+Example 2:
+
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+
+Example 3:
+
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: One of the valid triplet is (1, 4, 5), because nums[1] == 1 < nums[4] == 4 < nums[5] == 6.
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 5 * 105
+    -231 <= nums[i] <= 231 - 1
+
+ 
+Follow up: Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?
+
+*/
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int i_val = nums[0];
+        int j_val = INT_MAX;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > j_val) {
+                return true;
+            }
+            if (nums[i] > i_val) {
+                j_val = min(j_val, nums[i]);
+            } else if (nums[i] < i_val) {
+                i_val = nums[i];
+            }
+        }
+        return false;
+    }
+};
