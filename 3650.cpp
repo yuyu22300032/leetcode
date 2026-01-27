83154c4cc99a8217bca4a765c964e2df0f6f2dcc
YUYU <yuyu22300032@gmail.com>
2026-01-27 10:39:06 +0800

    add 3650

---
+++ 3650.cpp
@@ -0,0 +1,87 @@
+/*
+3650. Minimum Cost Path with Edge Reversals
+
+
+Description:
+
+You are given a directed, weighted graph with n nodes labeled from 0 to n - 1, and an array edges where edges[i] = [ui, vi, wi] represents a directed edge from node ui to node vi with cost wi.
+
+Each node ui has a switch that can be used at most once: when you arrive at ui and have not yet used its switch, you may activate it on one of its incoming edges vi → ui reverse that edge to ui → vi and immediately traverse it.
+
+The reversal is only valid for that single move, and using a reversed edge costs 2 * wi.
+
+Return the minimum total cost to travel from node 0 to node n - 1. If it is not possible, return -1.
+
+ 
+
+Example 1:
+
+Input: n = 4, edges = [[0,1,3],[3,1,1],[2,3,4],[0,2,2]]
+
+Output: 5
+
+Explanation:
+
+    Use the path 0 → 1 (cost 3).
+    At node 1 reverse the original edge 3 → 1 into 1 → 3 and traverse it at cost 2 * 1 = 2.
+    Total cost is 3 + 2 = 5.
+
+Example 2:
+
+Input: n = 4, edges = [[0,2,1],[2,1,1],[1,3,1],[2,3,3]]
+
+Output: 3
+
+Explanation:
+
+    No reversal is needed. Take the path 0 → 2 (cost 1), then 2 → 1 (cost 1), then 1 → 3 (cost 1).
+    Total cost is 1 + 1 + 1 = 3.
+
+ 
+
+Constraints:
+
+    2 <= n <= 5 * 104
+    1 <= edges.length <= 105
+    edges[i] = [ui, vi, wi]
+    0 <= ui, vi <= n - 1
+    1 <= wi <= 1000
+
+
+*/
+
+class Solution {
+    // edge from node[i], pair<toNode, cost>
+    vector<vector<pair<int, int>>> convertNodeEdges(int n, const vector<vector<int>>& edges) {
+        vector<vector<pair<int, int>>> nodeEdges(n);
+        for (int i = 0; i < edges.size(); i++) {
+            nodeEdges[edges[i][0]].push_back(pair<int, int>{edges[i][1], edges[i][2]});
+            nodeEdges[edges[i][1]].push_back(pair<int, int>{edges[i][0], edges[i][2] * 2});
+        }
+        return move(nodeEdges);
+    }
+public:
+    int minCost(int n, vector<vector<int>>& edges) {
+        vector<int> costToNode(n, INT_MAX);
+        costToNode[0] = 0;
+        vector<vector<pair<int, int>>> nodeEdges = move(convertNodeEdges(n, edges));
+        queue<int> searching;
+        searching.push(0);
+        while (!searching.empty()) {
+            int cur = searching.front();
+            searching.pop();
+            for (int i = 0; i < nodeEdges[cur].size(); i++) {
+                int cost = costToNode[cur] + nodeEdges[cur][i].second;
+                int node = nodeEdges[cur][i].first;
+                if (cost < costToNode[node]) {
+                    costToNode[node] = cost;
+                    searching.push(node);
+                }
+            }
+        }
+        if (costToNode.back() == INT_MAX) {
+            return -1;
+        }
+        return costToNode.back();
+    }
+};
