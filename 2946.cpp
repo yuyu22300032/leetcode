c3850d49ac87c9d6a19bbd0b6d7dacdd413a8f41
yuyu22300032 <70420076+yuyu22300032@users.noreply.github.com>
2026-03-27 08:35:05 +0800

    add 2946 (#47)

---
+++ 2946.cpp
@@ -0,0 +1,89 @@
+/*
+2946. Matrix Similarity After Cyclic Shifts
+
+
+Description:
+
+You are given an m x n integer matrix mat and an integer k. The matrix rows are 0-indexed.
+
+The following proccess happens k times:
+
+    Even-indexed rows (0, 2, 4, ...) are cyclically shifted to the left.
+
+    Odd-indexed rows (1, 3, 5, ...) are cyclically shifted to the right.
+
+Return true if the final modified matrix after k steps is identical to the original matrix, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: mat = [[1,2,3],[4,5,6],[7,8,9]], k = 4
+
+Output: false
+
+Explanation:
+
+In each step left shift is applied to rows 0 and 2 (even indices), and right shift to row 1 (odd index).
+
+Example 2:
+
+Input: mat = [[1,2,1,2],[5,5,5,5],[6,3,6,3]], k = 2
+
+Output: true
+
+Explanation:
+
+Example 3:
+
+Input: mat = [[2,2],[2,2]], k = 3
+
+Output: true
+
+Explanation:
+
+As all the values are equal in the matrix, even after performing cyclic shifts the matrix will remain the same.
+
+ 
+
+Constraints:
+
+    1 <= mat.length <= 25
+    1 <= mat[i].length <= 25
+    1 <= mat[i][j] <= 25
+    1 <= k <= 50
+
+ 
+*/
+
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        const int rowCnt = mat.size();
+        const int colCnt = mat[0].size();
+        const int shiftCnt = k % colCnt;
+        for (int i = 0; i < rowCnt; i += 2) {
+            for (int j = 0; j < colCnt; j++) {
+                int idx = j + shiftCnt;
+                if (idx >= colCnt) {
+                    idx -= colCnt;
+                }
+                if (mat[i][idx] != mat[i][j]) {
+                    return false;
+                }
+            }
+        }
+        for (int i = 1; i < rowCnt; i += 2) {
+            for (int j = 0; j < colCnt; j++) {
+                int idx = j - shiftCnt;
+                if (idx < 0) {
+                    idx += colCnt;
+                }
+                if (mat[i][idx] != mat[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
