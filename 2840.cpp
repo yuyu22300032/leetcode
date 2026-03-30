c6171a5a8b50fbf344c792f9263d54a048540efa
yuyu22300032 <70420076+yuyu22300032@users.noreply.github.com>
2026-03-30 19:38:38 +0800

    add 2840 (#49)

---
+++ 2840.cpp
@@ -0,0 +1,70 @@
+/*
+2840. Check if Strings Can be Made Equal With Operations II
+
+
+Description:
+
+You are given two strings s1 and s2, both of length n, consisting of lowercase English letters.
+
+You can apply the following operation on any of the two strings any number of times:
+
+    Choose any two indices i and j such that i < j and the difference j - i is even, then swap the two characters at those indices in the string.
+
+Return true if you can make the strings s1 and s2 equal, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s1 = "abcdba", s2 = "cabdab"
+Output: true
+Explanation: We can apply the following operations on s1:
+- Choose the indices i = 0, j = 2. The resulting string is s1 = "cbadba".
+- Choose the indices i = 2, j = 4. The resulting string is s1 = "cbbdaa".
+- Choose the indices i = 1, j = 5. The resulting string is s1 = "cabdab" = s2.
+
+Example 2:
+
+Input: s1 = "abe", s2 = "bea"
+Output: false
+Explanation: It is not possible to make the two strings equal.
+
+ 
+
+Constraints:
+
+    n == s1.length == s2.length
+    1 <= n <= 105
+    s1 and s2 consist only of lowercase English letters.
+
+
+*/
+
+class Solution {
+    int charToIdx(char c) {
+        return c - 'a';
+    }
+public:
+    bool checkStrings(string s1, string s2) {
+        vector<int> charCnt(26);
+        for (int i = 0; i < s1.size(); i += 2) {
+            charCnt[charToIdx(s1[i])] += 1;
+        }
+        for (int i = 0; i < s2.size(); i += 2) {
+            if (charCnt[charToIdx(s2[i])] == 0) {
+                return false;
+            }
+            charCnt[charToIdx(s2[i])] -= 1;
+        }
+        for (int i = 1; i < s1.size(); i += 2) {
+            charCnt[charToIdx(s1[i])] += 1;
+        }
+        for (int i = 1; i < s2.size(); i += 2) {
+            if (charCnt[charToIdx(s2[i])] == 0) {
+                return false;
+            }
+            charCnt[charToIdx(s2[i])] -= 1;
+        }
+        return true;
+    }
+};
