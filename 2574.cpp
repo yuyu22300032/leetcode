c345518dc0436ab9ccbc5380836bf6a3a016e303
YUYU <yuyu22300032@gmail.com>
2026-06-06 08:23:52 +0800

    add 2574

---
+++ 2574.cpp
@@ -0,0 +1,56 @@
+/*
+2574. Left and Right Sum Differences
+
+
+Description:
+
+You are given a 0-indexed integer array nums of size n.
+
+Define two arrays leftSum and rightSum where:
+
+    leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
+    rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
+
+Return an integer array answer of size n where answer[i] = |leftSum[i] - rightSum[i]|.
+
+ 
+
+Example 1:
+
+Input: nums = [10,4,8,3]
+Output: [15,1,11,22]
+Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
+The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
+
+Example 2:
+
+Input: nums = [1]
+Output: [0]
+Explanation: The array leftSum is [0] and the array rightSum is [0].
+The array answer is [|0 - 0|] = [0].
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 1000
+    1 <= nums[i] <= 105
+
+
+*/
+
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        vector<int> out(nums.size(), 0);
+        for (int i = 1; i < nums.size(); i++) {
+            out[i] = out[i - 1] + nums[i - 1];
+        }
+        int right_sum = 0;
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            right_sum += nums[i + 1];
+            out[i] = abs(out[i] - right_sum);
+        }
+        return out;
+    }
+};
