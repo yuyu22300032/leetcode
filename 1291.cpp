779cb34d8a2f51336405b9d5dd6114524c6640fc
yuyu22300032 <70420076+yuyu22300032@users.noreply.github.com>
2026-07-13 22:28:57 +0800

    add 1291 (#120)

---
+++ 1291.cpp
@@ -0,0 +1,54 @@
+/*
+1291. Sequential Digits
+
+
+Description:
+
+An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+
+Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+
+ 
+
+Example 1:
+
+Input: low = 100, high = 300
+Output: [123,234]
+
+Example 2:
+
+Input: low = 1000, high = 13000
+Output: [1234,2345,3456,4567,5678,6789,12345]
+
+ 
+
+Constraints:
+
+    10 <= low <= high <= 10^9
+
+*/
+
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        const vector<int> squentials = {12, 23, 34, 45, 56, 67, 78, 89,
+                                        123, 234, 345, 456, 567, 678, 789,
+                                        1234, 2345, 3456, 4567, 5678, 6789,
+                                        12345, 23456, 34567, 45678, 56789,
+                                        123456, 234567, 345678, 456789,
+                                        1234567, 2345678, 3456789,
+                                        12345678, 23456789,
+                                        123456789};
+        vector<int> out;
+        for (int i = 0; i < squentials.size(); i++) {
+            if (squentials[i] < low) {
+                continue;
+            }
+            if (squentials[i] > high) {
+                break;
+            }
+            out.push_back(squentials[i]);
+        }
+        return move(out);
+    }
+};
